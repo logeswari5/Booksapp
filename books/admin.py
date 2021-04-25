cc7096d45c15dd85652305db8dcc05e6095f69c3
logeswari5 <slogeshwari5@gmail.com>
2021-04-25 20:38:22 +0530

    Registering models in admin file

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Genre, Publisher
+
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Publisher)
